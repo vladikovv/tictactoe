daf442b3e92f51567876da8bf8137e4fba8eb48e
Stoycho Vladikov <stoycho-vladikov@mail.bg>
2023-05-28 15:47:29 +0300

    Fix game end bugs

--- script.rb
+++ script.rb
@@ -16,7 +16,7 @@ class Board
   end
 
   def pretty_print_board
-    puts 'Current board status: '
+    puts 'Currently the board looks like this: '
     puts '-----------'
     puts " #{@board[0][0]} | #{@board[0][1]} | #{@board[0][2]} "
     puts '---+---+---'
@@ -29,19 +29,19 @@ class Board
   def game_over?
     # check all the rows for a possible victory
     3.times do |i|
-      return true if @board[i].all? { |mark| mark == 'X' || mark == 'O' }
+      return true if @board[i].uniq.length == 1
     end
 
     # check all the columns for a possible victory
     3.times do |i|
-      return true if @board.transpose[i].all? { |mark| mark == 'X' || mark == 'O' }
+      return true if @board.transpose[i].uniq.length == 1
     end
 
     # check main diagonal for a possible victory
-    return true if 3.times.collect { |i| @board[i][i] }.all? { |mark| mark == 'X' || mark == 'O' }
+    return true if 3.times.collect { |i| @board[i][i] }.uniq.length == 1
 
     # check right diagonal for a possible victory
-    return true if right_diagonal_values.all? { |mark| mark == 'X' || mark == 'O'}
+    return true if right_diagonal_values.uniq.length == 1
 
     false
   end
@@ -84,51 +84,61 @@ end
 
 def game
   print "This will be a game of tic-tac-toe (commonly called sea chess in Bulgaria)) \n\n"
-  # sleep(2)
+
+  sleep(2)
   print 'Enter the name of the first player: '
-  player_one = Player.new('X', gets.chomp)
+  player_one = Player.new('x', gets.chomp)
   puts "#{player_one.name} will be using the 'X' mark!\n\n"
 
-  # sleep(2)
+
+  sleep(2)
   print 'Enter the name of the second player: '
-  player_two = Player.new('O', gets.chomp)
+  player_two = Player.new('o', gets.chomp)
   print "And #{player_two.name} will be using the 'O' mark!\n\n"
 
-  # sleep(2)
+
+  sleep(2)
   board = Board.new
-  print "Currently the board looks like this: \n\n"
   board.pretty_print_board
 
-  # sleep(2)
+
+  sleep(2)
   print "Each player must select a number between 0 and 8.\n"
-  # sleep(2)
+
+  sleep(2)
   print "That will put his mark on that specific position on the board.\n"
-  # sleep(2)
+
+  sleep(2)
   print "The board will be printed after each turn so each player can keep track.\n"
-  # sleep(2)
+
+  sleep(2)
   print("The game begins .....\n\n")
-  # sleep(5)
+
+  sleep(5)
   print("NOW!!!11!!\n\n")
-  # sleep(2)
+
+  sleep(2)
 
   game_over = false
   current_player = player_one
+  moves_count = 0
   until game_over
-    print("#{current_player.name}, pick a position on the board to place your mark: ")
+    print("#{current_player.name} (#{current_player.mark} player), pick a position on the board to place your mark: ")
     position = -1
     position = gets.chomp.to_i while position.negative? || position > 8
     board.place_mark(current_player.mark, position)
+    moves_count += 1
     board.pretty_print_board
     game_over = board.game_over?
     if game_over
       print("Game over! #{current_player.name} wins!")
       break
     end
-    if current_player.name == player_one.name
-      current_player = player_two
-    elsif current_player.name == player_two.name
-      current_player = player_one
+    if moves_count == 9
+      print("Game over! It's a draw!")
+      break
     end
+    current_player = current_player.name == player_one.name ? player_two : player_one
   end
 end
 
