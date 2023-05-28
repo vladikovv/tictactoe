000378986b8c82234b7e05631e8ec4cd9fdd7a8b
Stoycho Vladikov <stoycho-vladikov@mail.bg>
2023-05-28 15:24:40 +0300

    Add game loop and basic game functionality

--- script.rb
+++ script.rb
@@ -16,6 +16,7 @@ class Board
   end
 
   def pretty_print_board
+    puts 'Current board status: '
     puts '-----------'
     puts " #{@board[0][0]} | #{@board[0][1]} | #{@board[0][2]} "
     puts '---+---+---'
@@ -23,6 +24,26 @@ class Board
     puts '---+---+---'
     puts " #{@board[2][0]} | #{@board[2][1]} | #{@board[2][2]} "
     puts "-----------\n\n"
+  end
+
+  def game_over?
+    # check all the rows for a possible victory
+    3.times do |i|
+      return true if @board[i].all? { |mark| mark == 'X' || mark == 'O' }
+    end
+
+    # check all the columns for a possible victory
+    3.times do |i|
+      return true if @board.transpose[i].all? { |mark| mark == 'X' || mark == 'O' }
+    end
+
+    # check main diagonal for a possible victory
+    return true if 3.times.collect { |i| @board[i][i] }.all? { |mark| mark == 'X' || mark == 'O' }
+
+    # check right diagonal for a possible victory
+    return true if right_diagonal_values.all? { |mark| mark == 'X' || mark == 'O'}
+
+    false
   end
 
   private
@@ -36,6 +57,20 @@ class Board
     second_coord = coords[1]
     @board[first_coord][second_coord].is_a?(Integer)
   end
+
+  def right_diagonal_values
+    result = []
+    i = 0
+    while i < 3
+      j = 0
+      while j < 3
+        result.push(@board[i][j]) if (i + j) == 2
+        j += 1
+      end
+      i += 1
+    end
+    result
+  end
 end
 
 class Player
@@ -47,4 +82,54 @@ class Player
   end
 end
 
+def game
+  print "This will be a game of tic-tac-toe (commonly called sea chess in Bulgaria)) \n\n"
+  # sleep(2)
+  print 'Enter the name of the first player: '
+  player_one = Player.new('X', gets.chomp)
+  puts "#{player_one.name} will be using the 'X' mark!\n\n"
 
+  # sleep(2)
+  print 'Enter the name of the second player: '
+  player_two = Player.new('O', gets.chomp)
+  print "And #{player_two.name} will be using the 'O' mark!\n\n"
+
+  # sleep(2)
+  board = Board.new
+  print "Currently the board looks like this: \n\n"
+  board.pretty_print_board
+
+  # sleep(2)
+  print "Each player must select a number between 0 and 8.\n"
+  # sleep(2)
+  print "That will put his mark on that specific position on the board.\n"
+  # sleep(2)
+  print "The board will be printed after each turn so each player can keep track.\n"
+  # sleep(2)
+  print("The game begins .....\n\n")
+  # sleep(5)
+  print("NOW!!!11!!\n\n")
+  # sleep(2)
+
+  game_over = false
+  current_player = player_one
+  until game_over
+    print("#{current_player.name}, pick a position on the board to place your mark: ")
+    position = -1
+    position = gets.chomp.to_i while position.negative? || position > 8
+    board.place_mark(current_player.mark, position)
+    board.pretty_print_board
+    game_over = board.game_over?
+    if game_over
+      print("Game over! #{current_player.name} wins!")
+      break
+    end
+    if current_player.name == player_one.name
+      current_player = player_two
+    elsif current_player.name == player_two.name
+      current_player = player_one
+    end
+  end
+end
+
+game
